9dd98795a78f37e4985c8519c70cf7bcd02b4805
James <contact@james-wright.co.uk>
2021-03-25 21:27:54 +0000

    removes auto focus off form

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -9,15 +9,15 @@ export default function ContactForm() {
             {/* Name Input */}
             <div className="my-5 text-sm">
                 <label htmlFor="name" className="block text-black">Name</label>
-                <input type="text" autoFocus id="name" className="rounded-sm px-4 py-3 mt-3 focus:outline-none bg-gray-100 w-full" placeholder="" />
+                <input type="text"  id="name" className="rounded-sm px-4 py-3 mt-3 focus:outline-none bg-gray-100 w-full" placeholder="" />
             </div>
             <div className="my-5 text-sm">
                 <label htmlFor="email" className="block text-black">Email</label>
-                <input type="text" autoFocus id="email" className="rounded-sm px-4 py-3 mt-3 focus:outline-none bg-gray-100 w-full" placeholder="" />
+                <input type="text"  id="email" className="rounded-sm px-4 py-3 mt-3 focus:outline-none bg-gray-100 w-full" placeholder="" />
             </div>
             <div className="my-5 text-sm">
                 <label htmlFor="message" className="block text-black">Message</label>
-                <textarea autoFocus id="message" className="rounded-sm px-4 py-3 mt-3 focus:outline-none bg-gray-100 w-full resize-none" />
+                <textarea id="message" className="rounded-sm px-4 py-3 mt-3 focus:outline-none bg-gray-100 w-full resize-none" />
             </div>
             <Button label="Send Message"/>
         </form>
